90c4bae8e4cb5d0f585ee7fac7fc1f544a288902
gitiauxx <xavier.gitiaux@gmail.com>
2021-08-23 13:55:17 -0400

    Run with depth 10

--- examples/swap_count.py
+++ examples/swap_count.py
@@ -283,6 +283,6 @@ if __name__ == '__main__':
     os.makedirs(save_dir, exist_ok=True)
 
     results = {'succes': fitness == 0, 'circuit': circuit, 'cost': fitness}
-    rind = np.random.random(1)
+    rind = np.random.randint(0, 2**16)
     with open(f'{save_dir}/results_{tstamp}_{rind}.json', 'w') as file:
         json.dump(results, file)
